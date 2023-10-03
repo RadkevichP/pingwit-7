b97ae225d2749d92b2e6fa7fcc5d50ed8f01aa8d
Pavel Radkevich <pradkevich@eisgroup.com>
2023-10-03 10:07:27 +0300

    Lection 8 code examples

--- src/main/java/pl/pingwit/lec_8/point_1/GadgetMain.java
+++ src/main/java/pl/pingwit/lec_8/point_1/GadgetMain.java
@@ -7,6 +7,10 @@ public class GadgetMain {
 
         AppleGadget iphone11number1 = new AppleGadget("001A", GadgetTypes.SMARTPHONE, "iPhone 11", 64);
         AppleGadget iphone11number2 = new AppleGadget("001A", GadgetTypes.SMARTPHONE, "iPhone 11", 64);
+        AppleGadget iphone11number3 = iphone11number2;
+
+        System.out.println("Are links equal? " + (iphone11number1 == iphone11number2));
+        System.out.println("Are links equal? " + (iphone11number2 == iphone11number3));
 
         System.out.println(iphone11number1);
         System.out.println(iphone11number2);
